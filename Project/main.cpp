9b671e6136a560e38f7120389a8fc1ce52967822
Meinas Julien <julien.meinas@etudiant.univ-lille1.fr>
2020-11-17 00:03:05 +0100

    now result write in a file

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,11 +1,12 @@
 #include <gatb/gatb_core.hpp>
 #include <iostream>
+#include <fstream>
 #include <string.h>
 #include "Class/Filter/ClassFilter.hpp"
 #include "Class/Cleaning/ClassCleaning.hpp"
 #include "Class/Breakpoint/ClassBreakpoint.hpp"
 
-#define STR_URI_SEQUENCES   "-sequences"
+#define STR_URI_SEQUENCES               "-sequences"
 #define PERCENTAGE_SIMILARITY_FLT3      0.3
 
 /*Example: main -in gatb-core/gatb-core/test/db/reads1.fa -kmer-size 11 */
@@ -14,16 +15,22 @@
 // une première version d'affichage du resultat qui sera bien sûr modifié
 // par la suite
 void displayResult(std::map<std::string, BreakPoint*> mapResult, std::string nomPatient, int nbSeqence) {
-  std::cout << "\n\n\n\n\n\nCompte rendu du patient " << nomPatient << " : " << '\n';
-  std::cout << "Nombre de sequences FLT3 annalysé : " << nbSeqence << '\n';
+  ofstream m_resultFile;
+  nomPatient.replace(8, 4, "Results");
+  std::cout << "Rapport écrit dans le fichier " << nomPatient << '\n';
+  m_resultFile.open (nomPatient + ".txt");
+  m_resultFile << "Compte rendu des sequences " << nomPatient << " : " << "\n\n";
+  m_resultFile << "Nombre de sequences FLT3 annalysé : " << nbSeqence << "\n\n";
   int nbSequenceDefectueuse = 0;
   for(std::map<std::string, BreakPoint*>::iterator i=mapResult.begin(); i!=mapResult.end(); ++i) {
     nbSequenceDefectueuse += i->second->getOccurence();
   }
+  m_resultFile << "Nombre de duplications en tandem : " << nbSequenceDefectueuse << "\n";
+  m_resultFile << "Détaille de ces duplications : " << "\n";
   for(std::map<std::string, BreakPoint*>::iterator i=mapResult.begin(); i!=mapResult.end(); ++i) {
-    i->second->displayResult();
+    m_resultFile << i->second->displayResult();
   }
-  std::cout << (double)nbSequenceDefectueuse/nbSeqence*100
+  m_resultFile << (double)nbSequenceDefectueuse/nbSeqence*100
     << " % des cellules sont atteintes" << '\n';
 }
 
@@ -53,6 +60,8 @@ int main(int argc, char* argv[]) {
       // étape 3 : recherche des points d'arrets
       ClassBreakpoint breakpoint(clean.getResult());
       breakpoint.Breakpoint();
+
+
 
       displayResult(breakpoint.getMap(), options->getStr(STR_URI_SEQUENCES), clean.getResult().size());
 
